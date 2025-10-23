4e842331f20f33a25b470260421b0adf5066bc32
Bennett <bennett.mcelwee@sslab.co.nz>
2025-10-23 16:21:30 +1300

    Add skeleton recent-searches block

---
+++ blocks/build/blocks-manifest.php
@@ -0,0 +1,26 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'recent-searches' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'search-meter/recent-searches',
+		'version' => '0.1.0',
+		'title' => 'Recent Searches',
+		'category' => 'widgets',
+		'icon' => 'editor-ul',
+		'description' => 'A list of the most recent successful searches on the blog',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'search-meter',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ blocks/build/recent-searches/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cedcb944918590a57d57');
