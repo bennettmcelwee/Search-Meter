64e87cc8cb162e945d1af8103cfcbdc9ea3eb93a
Bennett <bennett.mcelwee@sslab.co.nz>
2025-11-11 18:11:18 +1300

    Add note about customising the title

--- blocks/build/popular-searches/index.asset.php
+++ blocks/build/popular-searches/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0963c079f45297a3632a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6ba878f72abdaeb69869');
