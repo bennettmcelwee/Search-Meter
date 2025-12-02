838da223d9cd99c1ec29c72c3c130c3795c29f58
Bennett McElwee <bennett@thunderguy.com>
2025-12-03 09:48:38 +1300

    Add note about customising the title

--- blocks/build/recent-searches/index.asset.php
+++ blocks/build/recent-searches/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b34ac4f8d161943a010e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '69f623931e75f9ac0788');
